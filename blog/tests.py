1211fe3eeb0ce86d18e77ee69572a20a455c1e1c
MJungJo <jmjung5400@gmail,com>
2022-05-07 01:58:17 +0900

    포스트 상세 페이지 테스트

--- blog/tests.py
+++ blog/tests.py
@@ -60,3 +60,39 @@ class TestView(TestCase):
 
         # 3.4 '아직 게시물이 없습니다'라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+# 포스트 상세 페이지 테스트 코드
+    def test_post_detail(self):
+        ## 1.1. 포스트가 하나 있다.
+        post_001 = Post.objects.create(
+            title='First Post',
+            content='Hello World. First Post.',
+        )
+
+        ## 1.2. 그 포스트의 url은 '/blog/1/'이다.
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # 2. 첫번째 포스트 상세 페이지 테스트
+        ## 2.1. 첫번째 포스트의 url로 접근하면 정산적으로 작동한다.(status code:200).
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        ## 2.2. 초스트 목록 페이징하 똑같은 내비게이션 바가 있다.
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Us', navbar.text)
+
+        ## 2.3. 첫번째 포스트의 제목이 웹브라우저 탭 타이틀에 들어 있다.
+        self.assertIn(post_001.title, soup.title.text)
+
+        ## 2.4. 첫번째 포스트의 제목이 포스트 영역에 있다.
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+
+        ## 2.5. 첫번째 포스트의 작성자(author)가 포스트 영역에 있다.(아직 공사중)
+
+        ## 2.6. 첫번째 포스트의 내용(content)이 포스트 영역에 있다
+        self.assertIn(post_001.content, post_area.text)
+        
